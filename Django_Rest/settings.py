8bcb4176b1762f66c40ed04ee371b09ccf455375
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-05-13 22:51:42 +0200

    filters and search

--- Django_Rest/settings.py
+++ Django_Rest/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'crispy_forms',
     'scraper',
     'jobIT',
     'rest_framework',
@@ -129,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50
 
